d22f77329a650e27f790b99c67450f7546cb3eaf
mkassy <maha.kasem@protonmail.com>
2024-08-21 18:07:09 -0400

    Import matplotlib's pyplot to plot dates and highs on a line graph.

--- Downloading Data/sitka_highs.py
+++ Downloading Data/sitka_highs.py
@@ -1,4 +1,7 @@
 import csv
+from datetime import datetime
+
+import matplotlib.pyplot as plt
 
 filename = 'data/sitka_weather_07-2018_simple.csv'
 with open(filename) as f:
@@ -11,10 +14,25 @@ with open(filename) as f:
     # for index, column_header in enumerate(header_row):
     #     print(index, column_header)
 
-    # First we'll get high temperatures from this file.
-    highs = []
+    # Get dates and high temperatures from this file.
+    dates, highs = [], []
     for row in reader:
+        current_date = datetime.strptime(row[2], '%Y-%m-%d')
         high = int(row[6])
+        dates.append(current_date)
         highs.append(high)
 
-    print(highs)
+
+# Plot the high temperatures.
+plt.style.use('seaborn-v0_8')
+fig, ax = plt.subplots()
+ax.plot(dates, highs, c='red')
+
+# Format plot.
+plt.title("Daily high temperatures, July 2018", fontsize=24)
+plt.xlabel('', fontsize=16)
+fig.autofmt_xdate()
+plt.ylabel("Temperature (F)", fontsize=16)
+plt.tick_params(axis='both', which='major', labelsize=16)
+
+plt.show()
